7bbfa344b852096042429d4895813c0010ec0d36
Edward Elric <SASUKE688848@gmail.com>
2022-11-11 08:40:59 +0000

    feat: next 13 and tailwind

---
+++ app/page.tsx
@@ -0,0 +1,71 @@
+import { Navbar } from "../components/navbar";
+import { MenuLinkContent } from "./types";
+
+export default function Home() {
+  const links: MenuLinkContent[] = [
+    {
+      id: "download",
+      url: "/",
+      title: "Download",
+    },
+    {
+      id: "docs",
+      url: "/docs",
+      title: "Docs",
+    },
+    {
+      id: "api",
+      url: "/api",
+      title: "API",
+    },
+    {
+      id: "playground",
+      url: "/playground",
+      title: "Playground",
+    },
+    {
+      id: "blog",
+      url: "/blog",
+      title: "Blog",
+    },
+    {
+      id: "community",
+      url: "/community",
+      title: "Community",
+    },
+  ];
+  return (
+    <>
+      <Navbar links={links}></Navbar>
+      <Body></Body>
+    </>
+  );
+}
+function Body() {
+  return (
+    <div className="absolute top-16 w-full">
+      <div className="mt-16 md:mt-32 lg:mt-40 mb-12">
+        <section className="flex justify-center">
+          <div className="max-w-1060 flex flex-col items-center px-5 sm:px-8 lg:box-content">
+            <h1 className="text-center max-w-[53rem] text-6xl">
+              Robust, Functional, Fully Typed JavaScript from the Future
+            </h1>
+            <h2 className="body-lg text-center text-gray-60 my-4 max-w-[40rem]">
+              RowScript is a robustly-typed functional language that compiles to
+              efficient and reliable JavaScript.
+            </h2>
+            <div className="mt-4 mb-2">
+              <a
+                className="select-none hover:cursor-pointer transition-colors duration-200 body-button focus:outline-none bg-blue-500 hover:bg-blue-700 text-white px-8 py-4 rounded-lg"
+                role="button"
+                href="/docs/manual/latest/installation"
+              >
+                Get started
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
+import { MenuLinkContent } from "../types";
+
+interface NavbarProps {
+  links: MenuLinkContent[];
+}
+
+export function Navbar({ links, ...props }: NavbarProps) {
+  return (
+    <header
+      className="static top-0 z-50 flex-shrink-0 py-4 bg-white md:sticky"
+      {...props}
+    >
+      <div className="container flex flex-col items-start justify-between px-6 mx-auto md:flex-row md:items-center">
+        <Link href="/" passHref className="text-lg font-bold">
+          RowScript
+        </Link>
+        {links ? <Menu items={links} /> : null}
+      </div>
+    </header>
+  );
+}
+
+function Menu({ items }: { items: MenuLinkContent[] }) {
+  return (
+    <ul
+      className="grid grid-flow-col gap-4 mx-auto mt-6 md:mt-0 auto-cols-auto md:auto-rows-auto md:gap-8 lg:gap-12"
+      data-cy="navbar-menu"
+    >
+      {items.map((item) => (
+        <MenuLink link={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function MenuLink({ link }: { link: MenuLinkContent }) {
+  const pathname = usePathname();
+
+  return (
+    <li>
+      <Link
+        href={link.url}
+        passHref
+        className={classNames(
+          "py-4 hover:underline text-sm md:text-base",
+          link.url === pathname ? "font-semibold" : "font-normal"
+        )}
+      >
+        {link.title}
+      </Link>
+    </li>
+  );
+}
