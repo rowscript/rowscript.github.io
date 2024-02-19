903e97109109b30cc9c923bed4789d879c44ee8f
kirraObj <c@skymc.me>
2024-02-19 12:39:37 +0800

    Explore and validate Docusaurus i18n mechanism through basic PoC changes.

---
+++ i18n/zh-Hans/docusaurus-plugin-content-blog/2023-05-05-open-sourcing.md
@@ -0,0 +1,18 @@
+---
+title: Open Sourcing RowScript
+authors: anqur
+tags: [ 新闻 ]
+---
+
+开源
+
+```bash
+npm install -D rowscript@latest
+```
+
+Currently, only a few constructs are provided with the compiler, in many forms:
+
+* Core syntax: Constructs embedded in the very core of RowScript, like the function type
+* Builtins: Constructs that are accessible in code, yet without documentation
+* Prelude: Constructs that are accessible without imports, with thorough documentation
+* Standard package: Constructs that are accessible with imports, bundled with the compiler
--- src/pages/zh-Hans/index.js
+++ src/pages/zh-Hans/index.js
@@ -15,18 +15,15 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/intro"
-            >
-              <Translate
-                id="homepage.title"
-                description="The title of the homepage"
-              />
-            </Link>
-          </div>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            wocaobusba 🚣
+          </Link>
         </div>
+      </div>
     </header>
   );
 }
