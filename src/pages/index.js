8de235e3decb7017eabed1567c31d208c235765a
kirraObj <c@skymc.me>
2024-03-04 19:13:20 +0800

    Fixed an issue where the tagline was not displaying correctly in i18n.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{indexContents.tagline.title}</p>
+        <p className="hero__subtitle">{indexContents.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
